4abfe7ce5d767d3290fefb5743a2aa3037c58ad2
James <james@jameshawkins.com>
2019-03-22 15:00:11 -0700

    Add leetcode problems

--- heaps_project/lib/is_heap.js
+++ heaps_project/lib/is_heap.js
@@ -1,6 +1,15 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
 
+    let maxHeap = true;
+    for (let i = 1; i < array.length; i++){
+        let currentNode = array[i];
+        let leftChild = array[i * 2] || -Infinity;
+        let rightChild = array[i * 2 + 1] || -Infinity;
+
+        if (currentNode < leftChild || currentNode < rightChild) maxHeap = false;
+    }
+    return maxHeap;
 }
 
 
